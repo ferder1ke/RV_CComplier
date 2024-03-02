54af8a8863063df9709e72958e3b19e91343fe23
ferder1ke <22s121106@stu.hit.edu.cn>
2024-03-02 09:17:30 +0800

    Adding terminator analysis and Eliminating whitespace functionality. hint :The double quotation marks represent the literal value of a string, while single quotation marks represent the literal value of a character.

--- main.c
+++ main.c
@@ -7,42 +7,118 @@
  ************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+
+typedef enum {
+    TK_PUNCT,
+    TK_NUM,
+    TK_EOF
+} TokenKind;
+
+typedef struct Token Token;
+
+struct Token{
+    TokenKind Kind;
+    Token* Next;
+    int Val;
+    int Len;
+    char* Pos; 
+};
+
+static void error(char* Fmt, ...) {
+    va_list VA;
+    va_start(VA, Fmt);
+    vfprintf(stderr, Fmt, VA);
+    fprintf(stderr, "\n");
+    va_end(VA);
+    exit(1);//Exception
+}
+static bool equal(Token* Tok, char* str){
+    return memcmp(Tok->Pos, str, Tok->Len) == 0 && str[Tok->Len] == '\0';
+}
+
+static Token* skip(Token* Tok, char* str) {
+    if(!equal(Tok, str))
+        error("expect '%s'", str);
+    return Tok->Next;
+} 
+
+static int getNum(Token* Tok) {
+    if(Tok->Kind != TK_NUM)
+        error("expect number!");
+    return Tok->Val;
+}
+
+static Token* genToken(TokenKind Kind, char* Start, char* End) {
+    Token* Tok = calloc(1, sizeof(Token));
+    Tok->Kind = Kind;
+    Tok->Pos = Start;
+    Tok->Len = End - Start;
+    return Tok;
+}
+
+static Token* parseToken(char* P) {
+   Token Head = {};
+   Token* cur = &Head;
+   while(*P != '\0') {
+       if(isspace(*P)) {
+            ++P;
+            continue;
+       }
+
+       if(isdigit(*P)) {
+            cur->Next = genToken(TK_NUM, P, P);
+            cur = cur->Next;
+            const char* oldPtr = P;
+            cur->Val = strtol(P, &P, 10);
+            cur->Len = P - oldPtr;
+            continue;
+       }
+
+       if(*P == '+' || *P == '-') {
+            cur->Next = genToken(TK_PUNCT, P, P + 1);
+            cur = cur->Next;
+            ++P;
+            continue;
+       }
+       
+       error("unexpected character %s", P);
+   }
+   
+   cur->Next = genToken(TK_EOF, P, P);
+   return Head.Next;
+}
+
 
 int main(int Argc, const char** Argv) {
 
     if(Argc != 2) {
         
-        fprintf(stderr, "%s: invalid number of Arguments!\n", Argv[0]);
-
+        error("%s: invalid number of Arguments!\n", Argv[0]);
         // 1 represent error
-        return 1;
     }
+    Token* Tok = parseToken(Argv[1]);
     printf("  .globl main\n");
-    // main段标签
     printf("main:\n");
-    
-    char* Pos = Argv[1];//args list pointer
     //num (op num) (op num)...
-    
-    // li为addi别名指令，加载一个立即数到寄存器中
-    // 传入程序的参数为str类型，因为需要转换为需要int类型，
-    // atoi为“ASCII to integer”
-    printf("  li a0, %ld\n", strtol(Pos, &Pos, 10));
-
-    while(*Pos != '\0') {
-        if(*Pos == '+') {
-            ++Pos;
-            printf("  addi a0, a0, %ld\n", strtol(Pos, &Pos, 10));
+    printf("  li a0, %d\n", getNum(Tok));
+    Tok = Tok->Next;
+    while(Tok->Kind != TK_EOF) {
+         if(equal(Tok, "+")) {
+            Tok = Tok->Next;
+            printf("  addi a0, a0, %d\n", getNum(Tok));
+            Tok = Tok->Next;
             continue;
-        }else if(*Pos == '-') {
-            ++Pos;
-            printf("  addi a0, a0, -%ld\n", strtol(Pos, &Pos, 10));
+         }else if(equal(Tok, "-")) {
+            Tok = Tok->Next;
+            printf("  addi a0, a0, -%d\n", getNum(Tok));
+            Tok = Tok->Next;
             continue;
-        }
-        fprintf(stderr, "unexpected character:'%c'\n", *Pos);
-        return 1;
+         }
     }
-    // ret为jalr x0, x1, 0别名指令，用于返回子程序
     printf("  ret\n");
 
     return 0;
